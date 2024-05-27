04e13ca9cf237d7667e16ad4d0105c663f391325
BasileDM <xnosx@hotmail.fr>
2024-05-27 14:18:53 +0200

    Add client project to solution + HTTP request learn.microsoft follow along

---
+++ SystemInfoClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using SystemInfoClient;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+var repositories = await ProcessRepositoriesAsync(client);
+foreach (var repository in repositories) {
+    Console.WriteLine($"Name: {repository.Name}");
+    Console.WriteLine($"Homepage: {repository.Homepage}");
+    Console.WriteLine($"GitHub: {repository.GitHubHomeUrl}");
+    Console.WriteLine($"Description: {repository.Description}");
+    Console.WriteLine($"Watchers: {repository.Watchers:#,0}");
+    Console.WriteLine($"Last push: {repository.LastPush}");
+    Console.WriteLine();
+}
+
+static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client) {
+    await using Stream stream =
+        await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+    var repositories =
+        await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+
+    return repositories ?? [];
+}
