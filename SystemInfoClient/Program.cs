41d6b528c3f87b98fe0919234bc5f8281b610986
BasileDM <xnosx@hotmail.fr>
2024-05-27 16:24:04 +0200

    Add API info basic processing task

--- SystemInfoClient/Program.cs
+++ SystemInfoClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using SystemInfoClient.Models;
 
@@ -8,34 +9,39 @@ client.DefaultRequestHeaders.Accept.Add(
     new MediaTypeWithQualityHeaderValue("application/json")); // TODO: modify
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter"); // TODO: modify
 
+
 string host = "https://localhost";
 Console.Write("Port ? ");
 string? port = Console.ReadLine();
 string apiRoute = "/Systeminfo/all";
 var fullAddress = host + ":" + port + apiRoute;
-Console.WriteLine(fullAddress);
 
-var systemInfo = client.GetAsync(fullAddress).Result;
-Console.WriteLine(systemInfo);
+Console.WriteLine($"Fetching at: {fullAddress}");
 
+await ProcessSystemInfoAsync(client, fullAddress);
 Console.ReadLine();
 
-var repositories = await ProcessRepositoriesAsync(client);
-foreach (var repository in repositories) {
-    Console.WriteLine($"Name: {repository.Name}");
-    Console.WriteLine($"Homepage: {repository.Homepage}");
-    Console.WriteLine($"GitHub: {repository.GitHubHomeUrl}");
-    Console.WriteLine($"Description: {repository.Description}");
-    Console.WriteLine($"Watchers: {repository.Watchers:#,0}");
-    Console.WriteLine($"Last push: {repository.LastPush}");
-    Console.WriteLine();
+static async Task ProcessSystemInfoAsync(HttpClient client, string address) {
+    var systemInfo = await client.GetStringAsync(address);
+    Console.WriteLine(systemInfo);
 }
 
-static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client) {
-    await using Stream stream =
-        await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-    var repositories =
-        await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+//var repositories = await ProcessRepositoriesAsync(client);
+//foreach (var repository in repositories) {
+//    Console.WriteLine($"Name: {repository.Name}");
+//    Console.WriteLine($"Homepage: {repository.Homepage}");
+//    Console.WriteLine($"GitHub: {repository.GitHubHomeUrl}");
+//    Console.WriteLine($"Description: {repository.Description}");
+//    Console.WriteLine($"Watchers: {repository.Watchers:#,0}");
+//    Console.WriteLine($"Last push: {repository.LastPush}");
+//    Console.WriteLine();
+//}
 
-    return repositories ?? [];
-}
+//static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client) {
+//    await using Stream stream =
+//        await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+//    var repositories =
+//        await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+
+//    return repositories ?? [];
+//}
