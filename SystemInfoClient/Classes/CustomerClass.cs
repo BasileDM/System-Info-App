2126523a8bd61662a53e30a93dc22f0f17a65b2e
BasileDM <xnosx@hotmail.fr>
2024-05-29 16:07:47 +0200

    Add customer class

---
+++ SystemInfoClient/Classes/CustomerClass.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemInfoClient.Classes {
+    internal class CustomerClass {
+        public string Name { get; set; }
+        public List<MachineClass> Machines { get; set; } = [];
+
+        public CustomerClass() {
+            while (true) {
+                Console.Write("Entrer le nom du client : ");
+                Name = Console.ReadLine();
+                if (Name == null) {
+                    break;
+                }
+                Console.WriteLine("Veuillez entrer une valeur");
+            }
+        }
+    }
+}
