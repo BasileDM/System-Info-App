4cce37e82a21e540eb3c15babfc327e0672c2be9
BasileDM <xnosx@hotmail.fr>
2024-05-31 10:59:43 +0200

    Add http post request logic in main function

--- SystemInfoClient/SystemInfoClient.cs
+++ SystemInfoClient/SystemInfoClient.cs
@@ -1,67 +1,46 @@
 ﻿using System.Net;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Runtime.Versioning;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using SystemInfoClient.Classes;
 using System.Diagnostics;
 
-namespace SystemInfoClient;
 
-internal class SystemInfoClient {
+namespace SystemInfoClient {
 
-    [SupportedOSPlatform("windows")]
-    public static async Task Main(string[] args) {
+    internal class SystemInfoClient {
 
-        var customer = new CustomerClass();
-        //customer.LogInfo();
+        public static async Task Main(string[] args) {
 
-        using HttpClient client = new();
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
-        client.DefaultRequestHeaders.Add("User-Agent", "Systeminfo App Client");
+            client.DefaultRequestHeaders.Add("User-Agent", "Systeminfo App Client");
 
-        string protocol = "https";
-        string host = "localhost";
-        string port = "7056";
-        string route = "/Systeminfo/drives";
+            var route = "https://localhost:7056/api/CustomerModels";
+            var customer = new CustomerClass();
+            //customer.LogInfo();
 
-        var fullAddress = protocol + "://" + host + ":" + port + route;
+            JsonSerializerOptions jsonOptions = new() { WriteIndented = true };
+            var json = JsonSerializer.Serialize(customer, jsonOptions);
 
-        Console.WriteLine($"Posting at: {fullAddress}");
-        Console.WriteLine();
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-        var drives = await ProcessDrivesAsync(client, fullAddress);
+            Console.WriteLine(json);
 
-        foreach (var drive in drives) {
-            Console.WriteLine($"Name: {drive.Name}");
-            Console.WriteLine($"Label: {drive.Label}");
-            Console.WriteLine($"Drive type: {drive.DriveType}");
-            Console.WriteLine($"Drive format: {drive.DriveFormat}");
-            Console.WriteLine($"Total size: {drive.TotalSize:#,0} bits");
-            Console.WriteLine($"Available space: {drive.AvailableFreeSpace:#,0} bits");
-            Console.WriteLine($"Total free space: {drive.TotalFreeSpace:#,0} bits");
-            Console.WriteLine($"Free space percentage: {drive.SpacePercentageStr}");
-            Console.WriteLine();
+            //var response = await client.PostAsync(route, content);
+
+            //if (response.IsSuccessStatusCode) {
+            //    Console.WriteLine($"Customer data sent successfully. {response.StatusCode}");
+            //} else {
+            //    Console.WriteLine($"Post request failed: {response.StatusCode}");
+            //}
+            
         }
-        Console.ReadLine();
-
-        // Async methods
-
-        static async Task<List<DriveModel>> ProcessDrivesAsync(HttpClient client, string address) {
-            await using Stream stream =
-                await client.GetStreamAsync(address);
-            var drives =
-                await JsonSerializer.DeserializeAsync<List<DriveModel>>(stream);
-
-            return drives ?? [];
-        }
-
     }
 
-    public static string? FindVsVersion() {
-        string exePath = "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe";
-        string? version = FileVersionInfo.GetVersionInfo(exePath).FileVersion;
-        return version;
-    }
+
+
 }
