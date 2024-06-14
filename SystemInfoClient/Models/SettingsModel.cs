d257f722f0f646b4d6e14849f9ad7b87579e6514
BasileDM <xnosx@hotmail.fr>
2024-06-14 14:12:20 +0200

    Change settings deserialization model + Adapt appList log method

--- SystemInfoClient/Models/SettingsModel.cs
+++ SystemInfoClient/Models/SettingsModel.cs
@@ -3,9 +3,13 @@
     internal class SettingsModel
     {
         public string? CustomerId { get; set; }
-
         public string? ApiUrl { get; set; }
+        public Dictionary<string, ApplicationSettings>? ApplicationsList { get; set; }
+    }
 
-        public Dictionary<string, string>? Applications { get; set; }
+    internal class ApplicationSettings
+    {
+        public string? Id { get; set; }
+        public string? Path { get; set; }
     }
 }
